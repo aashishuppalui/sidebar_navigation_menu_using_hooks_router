aaa0c0b2e6f367671e9ce3aa5ee6f3c580c9ff8e
aashishuppal <rogerux09@gmail.com>
2020-10-16 00:47:17 +0530

    sidebar navigation menu using hooks router finished

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <div className="home">
+                <h1>Home</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home
